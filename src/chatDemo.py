e3ad2047a4b65de829d9ecc4a76aeecbf5692ea1
Richard DeVaul <richard.devaul@gmail.com>
2023-12-04 14:33:44 -0800

    further revisions, addition of demo framework

---
+++ src/chatDemo.py
@@ -0,0 +1,139 @@
+# A very simple demo chatbot that talks by sockets to a local TCP
+# client.
+
+from chatstate import Reply, User, ChatState, makeWrapper
+
+import re
+import colorama
+from colorama import Fore, Back, Style
+
+print("=================================================")
+print("simple demo chat system that authneticates")
+print("a user and echos their text till a /quit command ")
+print("==> Created by Rich @rdevaul")
+print("=================================================")
+print()
+
+      
+## Define the state machine for chat
+
+stateMachine = [ 'undefined','auth', 'echo' ]
+
+## Define the legal transitions between states
+
+graph = {
+    'undefined': [ 'auth' ],
+    'auth': [ 'auth', 'echo', 'undefined' ],
+    'echo': [ 'echo', 'undefined' ]
+}
+
+## State Transition Functions
+
+# NOTE: state transition functions get passed client_id. If you are
+# working with a seperate global user identifier, you need to perform
+# a mapping operation to get that. For now, we assume they are
+# synonymous
+
+def undefined(cs,client_id,state):
+    """function for 'undefined' state"""
+    if not cs.checkUser(client_id):
+        newuser = User("global:"+client_id,client_id,"anon-kun", {})
+        cs.addUser(newuser)
+    return Reply("Plese provide your authoriation code"), 'auth'
+
+def isAuth(code):
+    """ code authorization utilty function"""
+    if code.strip() == '12345': # check for secret code
+        return True
+    return False
+
+def auth(cs,client_id,state):
+    """ function for 'auth' state - will verify code, or ask again"""
+    # extract the user's most recent reply
+    message = state["reply"].content
+    
+    if not cs.checkUser(client_id):
+        raise Error("user doesn't exist - should never happen")
+
+    # count authorization attempts
+    counter = 0
+    # get an auth attempt counter, if present.
+    if 'authcounter' in state:
+        counter = state['authcounter']
+    # increment
+    counter += 1
+    # store the results in the user's state dictionary 
+    cs.updateUserState(client_id, {'authcounter': counter})
+    
+    reply = None
+    rval = None
+    if counter > 5:             # too many tries
+        reply = Reply('Maximum sign-in attempts exceeded')
+        rval = 'undefined'
+    elif isAuth(message):
+        reply = Reply("Thanks for signing in. What can I do for you?")
+        rval = 'echo'
+    else:
+        reply = Reply("I'm sorry, I didn't recongize that code. You have {5-counter} attempts left. Could you try again?")
+        rval = 'auth'
+    return reply, rval
+      
+def echo(cs,client_id,state):
+    """function for 'echo' state - will simply echo back the user's
+     message endlessly. NOTE: requires a command wrapper to transition
+     to another state.
+    
+    """
+    # extract the user's most recent reply
+    message = state["reply"].content
+    reply = Reply(f'echo: {message} (/quit to exit)')
+    rval = 'echo'
+    if not cs.checkUser(client_id):
+        raise Error("user doesn't exist - should never happen")
+    return reply, rval
+
+## slash commands for bot - define "quit" and "help". Note that slash
+## commands can result in state transition or simply result in side
+## effects without a state transition.
+
+def quitfunc(message,cs,client_id,state):
+    reply = Reply('Signing you out.  Goodby!')
+    rval = 'undefined'
+    return reply, rval
+
+quit_cmd = r'^\s*/quit'         # match /quit
+
+def helpfunc(message,cs,client_id,state):
+    reply=Reply( """
+This is a simple demo bot.
+The only commands available after you sign in are:
+    /help — produces this message, and
+    /quit — signs you out.
+    """)
+    # don't update the state transition
+    rval = state['oldstate']
+    return reply, rval
+
+help_cmd = r'^\s*/help'         # match /help
+
+# Wrap the echo state transition function with the slash command
+# wrapper that implements /help and /quit
+echo = makeWrapper([quit_cmd, help_cmd],
+                   [quitfunc, helpfunc], echo)
+
+# create the mapping between states and functions
+
+functions = { 'undefined': undefined,
+              'auth': auth,
+              'echo': echo }
+
+# create the ChatState instance
+
+chatstate = ChatState(stateMachine,
+                      graph,
+                      functions)
+
+## Verify the state machine
+if not chatstate.verifyState():
+    raise Exception('Incomplete initialization of chatstate')
+
--- src/chatstate.py
+++ src/chatstate.py
@@ -1,13 +1,36 @@
-import asyncio
 ## State machine support for chat
+"""Simple framework for asynchronous, event-driven, stateful chatbot
+interaction.  Created by Rich DeVaul, @rdevaul on Github, X
+
+
+"""
+
+import asyncio
 
 class Reply:
     """Class representing a reply object, media type support is subject to chat client"""
+    replytypes = ['text','audio','image','video']
     def __init__(self,
                  content,
                  reply_type = "text"):
         self.content = content
-        self.reply_type = reply_type
+        if reply_type not in self.replytypes:
+            raise Exception(f'bad reply_type in Reply init: {reply_type}')
+        self.reply_type = reply_typ
+
+    def asdict(self):
+        """Produce Python3 dictionary representation of the Reply instance"""
+        return {'tag': 'reply',
+                'content': self.content,
+                'reply_type': self.reply_type }
+        
+def dict2reply(dct):
+    """ convert from a Python3 dict to a Reply object instance"""
+    if 'tag' not in dct or dct['tag'] != 'reply':
+        raise Exception('bad dictionary passed to dict2reply')
+    
+    return Reply(dct["content"],
+                 dct["reply_type"])
 
 class User:
     """Class representing a chat user"""
@@ -21,10 +44,32 @@ class User:
         self.name = name
         self.state = state
         
+    def asdict(self):
+        """Produce Python3 dictionary representation of a User instance"""
+        return {'tag': 'user',
+                'user_id': self.user_id,
+                'client_id': self.client_id,
+                'name': self.name,
+                'state': self.state }
 
+def dict2user(dct):
+    """ convert from a Python3 dict to a User object instance"""
+    if 'tag' not in dct or dct['tag'] != 'user':
+        raise Exception('bad dictionary passed to dict2user')
+    
+    return User(dct["user_id"],
+                dct["client_id"],
+                dct["name"],
+                dct["state"])
+    
 class ChatState:
     """Class instance represents chat state machine"""
+    
     async def default_send(client_id,message):
+        """placeholder user message function, which is just a print
+        statement. You will almost always want to use something else.
+
+        """ 
         print(f"{x}: {y.content}")
     
     def __init__(self,
@@ -77,12 +122,21 @@ class ChatState:
         return True
 
     def verifyState(self):
+        """Verify that the ChatState instance is conforming"""
         return self.verifyGraph() and self.verifyFunctions()
 
-    def checkState(self,client_id):
+    def checkUser(self,client_id):
+        """Check to see if a client_id user exists in active users"""
         return client_id in self.users and client_id in self.states
 
-    def updateState(self,client_id,nstate):
+    def addUser(self,newuser: User):
+        """Add a new user"""
+        client_id = newuser.client_id
+        self.users[client_id] = newuser
+        self.states[client_id] = undefined
+
+    def updateUserState(self,client_id,nstate):
+        """ use a revised state to update the existing user state"""
         self.states[client_id] = {**self.states[client_id], **nstate}
 
     async def nextInput(self,client_id,in_reply):
@@ -102,17 +156,74 @@ class ChatState:
 
         # record the state before transition
         state['oldstate'] = state['nextstate']
+        # record the reply, convert to Reply object
         state['reply'] = Reply(in_reply)
+        # perform state update, if needed
         self.states[client_id] = { **self.states[client_id], **state}
+        # invoke state transition function, get reply for user and return
+        # value which is a string indicating next state.
         reply, rval = fn(self,client_id,state)
+        # debugging
         print(f'graph: {self.graph[nextstate]}')
+        # check to make sure next state is legal
         if rval not in self.graph[nextstate]:
             raise Exception(f'Bad state transition from {nextstate} to {rval}')
+        # update history
         if not 'history' in self.states[client_id].keys():
             self.states[client_id]['history'] = [ in_reply, reply.content ]
         else:
             self.states[client_id]['history'] += [ in_reply, reply.content ]
-        # update new state
+        # update new state with return value from state transition funct.
         self.states[client_id]['nextstate'] = rval
-        # self.states[client_id] = { **self.states[client_id], **state}
+        # send the message to the user
         await self.sendMessage(client_id,reply)
+
+
+## Utility functions for creating command-processing wrapper functions
+
+def process_commands(input_string,
+                     command_regex_list, command_functions,
+                     cs=None,client_id=None,state={}):
+    """
+    Process user input strings and call corresponding functions based on matched commands.
+
+    Parameters:
+    - input_string: The user input string to process.
+    - command_regex_list: A list of regular expressions corresponding to command strings.
+    - command_functions: A list of functions corresponding to the commands.
+
+    Returns:
+    - return value of matching function, or False if no match
+    """
+    for regex, func in zip(command_regex_list, command_functions):
+        match = re.match(regex, input_string)
+        if match:
+            remaining_input = input_string[match.end():].strip()
+            return func(remaining_input,cs,client_id,state)
+    return False
+
+def makeWrapper(regex_list,function_list,state_function):
+    """take a list of command-matching regular expressions, a list of
+    corresponding command functions, and state-transition function to
+    wrap, and return a new function that is the original state
+    transition function but wrapped with the process_command function.
+
+    If there is a match to any command regex, the corresponding
+    command function will be invoked and become the return value,
+    superceeding the state transition function.
+
+
+    If no command is matched, the state transition function is invoked.
+
+    """
+
+    def wrapper(cs,client_id,state):
+        message = state["reply"].content
+        rval = process_command(message,
+                               regex_list, function_list,
+                               cs,client_id,state)
+        if not rval:
+            return state_function(cs,client_id,state)
+        return rval
+
+    return wrapper
